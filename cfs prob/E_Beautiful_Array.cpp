07dfe62c51d201578889f2a965bd52b9a9a85b63
Anuj Gupta <davgu12345@gmail.com>
2025-12-22 17:50:10 +0530

    Delete cfs prob directory

--- cfs prob/E_Beautiful_Array.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int a,b;cin>>a>>b;
-    cout<<3<<endl;
-    cout<<3*a-2*b<<" "<<b<<" "<<b;
-    return 0;
-}
--- cfs prob/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# Plot phase portrait
-# axes[1].plot(prey, predator, 'b-')
-# axes[1].set_xlabel('Prey Population')
-# axes[1].set_ylabel('Predator Population')
-# axes[1].set_title('Phase Portrait')
-# axes[1].grid(True)
