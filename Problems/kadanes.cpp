f3aefb429d37472b4f41ba4cc66a5d0c0592b89d
Anuj Gupta <davgu12345@gmail.com>
2026-01-05 23:43:27 +0530

    5/1/26

---
+++ Problems/kadanes.cpp
@@ -0,0 +1,32 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    #define fast_io                       \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(NULL);                    \
+        cout.tie(NULL);
+    typedef long long ll;
+    typedef vector<int> vi;
+    typedef pair<int, int> pii;
+
+    // const int MOD = 1e9 + 7;
+
+    class Solution
+    {
+    public:
+        int maxSubarraySum(vector<int> &arr)
+        {
+            // Code here
+            int currsum = 0, maxsum = INT_MIN;
+            for (int i = 0; i < arr.size(); i++)
+            {
+                currsum += arr[i];
+                maxsum = max(currsum, maxsum);
+                if (currsum < 0)
+                {
+                    currsum = 0;
+                }
+            }
+            return maxsum;
+        }
+    };
