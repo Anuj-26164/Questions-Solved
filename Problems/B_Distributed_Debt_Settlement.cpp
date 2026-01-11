f96e4ece23eb9a7ec947845b9b82737f1fc66a43
Anuj Gupta <davgu12345@gmail.com>
2026-01-12 00:24:43 +0530

    11/1/26

---
+++ Problems/B_Distributed_Debt_Settlement.cpp
@@ -0,0 +1,158 @@
+// Qs: B_Distributed_Debt_Settlement
+// Time: 14:20:22
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vl vector<long long>
+#define all(x) begin(x), end(x)
+#define pb push_back
+#define int long long
+#define ll long long
+#define f(i, k, n) for (int i = k; i < n; i++)
+#define nl endl
+#define vin(v, n)               \
+    vl v(n);                    \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> v[i];            \
+    }
+#define vout(v, i, k, n)            \
+    do                              \
+    {                               \
+        for (int i = k; i < n; i++) \
+            cout << v[i] << " ";    \
+        cout << "\n";               \
+    } while (0)
+#define ff first
+#define ss second
+#define MOD 1000000007LL
+const ll INF = 4e18;
+template <class T, class Container = vector<T>>
+using maxheap = priority_queue<T, Container, less<T>>;
+template <class T, class Container = vector<T>>
+using minheap = priority_queue<T, Container, greater<T>>;
+#define DP1(v, n) vector<long long> v((n) + 1, -1)
+#define DP2(v, n, m) vector<vector<long long>> v((n) + 1, vector<long long>((m) + 1, -1))
+#define pll pair<long long, long long>
+#define debug(x) cerr << #x << " = " << x << nl;
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (size_t i = 0; i < v.size(); i++)
+        os << (i ? ", " : "") << v[i];
+    return os << "]";
+}
+
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T, U> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &s)
+{
+    os << "{";
+    bool f = true;
+    for (auto &x : s)
+        os << (f ? "" : ", ") << x, f = false;
+    return os << "}";
+}
+
+template <typename T, typename U>
+ostream &operator<<(ostream &os, const map<T, U> &m)
+{
+    os << "{";
+    bool f = true;
+    for (auto &x : m)
+        os << (f ? "" : ", ") << x, f = false;
+    return os << "}";
+}
+
+void yes()
+{
+    cout << "YES" << endl;
+}
+
+void no()
+{
+    cout << "NO" << endl;
+}
+
+int msb(int mask)
+{
+    if (mask == 0)
+        return -1;
+    return 63 - __builtin_clzll(mask);
+}
+#define lsb(mask) __builtin_ctzll(mask)
+#define cntsetbit(mask) __builtin_popcountll(mask)
+#define checkbit(mask, bit) ((mask >> bit) & 1ll)
+#define onbit(mask, bit) ((mask) | (1LL << (bit)))
+#define offbit(mask, bit) ((mask) & ~(1LL << (bit)))
+#define changebit(mask, bit) ((mask) ^ (1LL << bit))
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<long long> balance(n + 1, 0);
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        long long x;
+        cin >> a >> b >> x;
+        balance[a] -= x;
+        balance[b] += x;
+    }
+
+    vector<pair<int, long long>> d, c;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (balance[i] < 0)
+            d.push_back({i, -balance[i]});
+        else if (balance[i] > 0)
+            c.push_back({i, balance[i]});
+    }
+
+    vector<tuple<int, int, long long>> ans;
+
+    int i = 0, j = 0;
+    while (i < (int)d.size() && j < (int)c.size())
+    {
+        long long x = min(d[i].second, c[j].second);
+
+        ans.emplace_back(d[i].first, c[j].first, x);
+
+        d[i].second -= x;
+        c[j].second -= x;
+
+        if (d[i].second == 0)
+            i++;
+        if (c[j].second == 0)
+            j++;
+    }
+
+    cout << ans.size() << '\n';
+    for (auto &t : ans)
+    {
+        cout << get<0>(t) << " "
+             << get<1>(t) << " "
+             << get<2>(t) << '\n';
+    }
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
